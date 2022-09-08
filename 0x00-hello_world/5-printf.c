6654aa78869d7f4e2776d829e24db4ef71feae56
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-09-08 15:00:09 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.>
+
+/**
+ * main - print the string in the put function
+ *
+ * Description: using the main function
+ * this program print "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a peice of art,\n");
+}
