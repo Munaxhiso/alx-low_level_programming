5730c979973651a1174ba2dda928c815346c664f
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-09-09 12:49:31 -0700

    random numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,11 +19,11 @@ l = n % 10;
 
 if (l > 5)
 {
-	printf("Last digit of %d is %d and is greater than 5/n", n, l);
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
 else if (1 == 0)
 {
-	printf("Last digit of %d is %d and is 0/n", n, l);
+	printf("Last digit of %d is %d and is 0\n", n, l);
 }
 else
 {
