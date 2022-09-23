521f7d99f28b4840bf6908dc77e4cb3364f6eb5d
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-09-23 14:23:53 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -16,11 +16,17 @@ char *_strncpy(char *dest, char *scr, int n)
 
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-
-	for ( ; i < n; i++)
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src [i];
+		i++;
+	}
+	while (i < n)
+	{
 		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
