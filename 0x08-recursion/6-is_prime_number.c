3b0d0cf1f2d928da7aeb91730344a7ed23764a11
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-09-29 19:08:32 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,6 @@
 int is_prime_number(int n)
 {
 	int start = n / 2;
-	
 	if (n <= 1)
 		return (0);
 	return (is_prime(n, start));
@@ -20,7 +19,7 @@ int is_prime_number(int n)
  * @n: number to be checked
  * @start: number to start checking from
  *
- * return: 1 if n is prime, 0 otherwise
+ * Return: 1 if n is prime, 0 otherwise
  */
 int is_prime(int n, int start)
 {
