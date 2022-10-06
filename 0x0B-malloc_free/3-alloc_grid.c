c30863e8f012adb28948dc13750ef6f938c9db91
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-10-05 17:02:18 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,26 +18,26 @@ int **alloc_grid(int width, int height)
 	array = (int **) malloc(sizeof(int *) * height);
 	if (array != NULL)
 	{
-		 for (; i < height; i++)
-		 {
-			 array[i] = (int *) malloc(sizeof(int) * width);
-			 if (array[i] != NULL)
-			 {
-				 for (j = 0; j < width; j++)
-					 array[i][j] = 0;
-			 }
-			 else
-			 {
-				 while (i >= 0)
-				 {
-					 free(array[i]);
-					 i--;
-				 }
-				 free(array);
-				 return (NULL);
-			 }
-		 }
-		 return (array);
+		for (; i < height; i++)
+		{
+			array[i] = (int *) malloc(sizeof(int) * width);
+			if (array[i] != NULL)
+			{
+				for (j = 0; j < width; j++)
+					array[i][j] = 0;
+			}
+			else
+			{
+				while (i >= 0)
+				{
+					free(array[i]);
+					i--;
+				}
+				free(array);
+				return (NULL);
+			}
+		}
+		return (array);
 	}
 	else
 	{
