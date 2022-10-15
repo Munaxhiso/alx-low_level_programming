3a8c6fcf010a32b7128c033405bd7e56ab9e951e
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-10-14 18:25:14 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,8 +21,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		num = va_arg(valist, int);
 		printf("%d", num);
 		if (i < n - 1 && separator)
-			print("%s", separator);
+			printf("%s", separator);
 	}
-	printn("\n");
+	printf("\n");
 	va_end(valist);
 }
