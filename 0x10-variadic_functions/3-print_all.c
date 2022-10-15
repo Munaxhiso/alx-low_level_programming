d6ffc31cc20cd9da582fb96d09fd59f6aa555796
Munaxhiso <okorafornwosumunachi@gmail.com>
2022-10-14 19:44:33 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -21,22 +21,22 @@ void print_c(va_list c)
  */
 void print_s(va_list s)
 {
-	printf("%c", va_arg(c, int));
-}
-
-/**
- * print_s - prints a string
- * @s: string to print
- *
- * Return: void
- */
-void print_s(va_list s)
-{
 	char *str = va_arg(s, char *);
 
 	if (str == NULL)
 		str = "(nil)";
 	printf("%s", str);
+}
+
+/**
+ * print_i - prints an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
 }
 
 /**
